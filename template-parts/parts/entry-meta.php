a674736a55f39c186830931952b84301262612d0
ughimire <me.umeshuser@gmail.com>
2020-09-01 17:11:00 +0545

    update

--- template-parts/parts/entry-meta.php
+++ template-parts/parts/entry-meta.php
@@ -1,6 +1,6 @@
 <?php if ('post' === get_post_type()) {
 	if (!has_post_format('link')) {
-		$mnp_post_from = is_archive() ? 'archive' : 'single';
+		$mnp_post_from = is_archive() || is_home() ? 'archive' : 'single';
 		?>
 		<div class="entry-meta magazinenp-parts-item">
 			<?php magazinenp_posted_on($mnp_post_from); ?>
